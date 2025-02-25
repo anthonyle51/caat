d9071d078c43b0d6b515b87501fdd93daff1c442
anthonyle51 <anthonyle20045@gmail.com>
2025-02-25 14:55:28 -0500

    structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+    import Header from './Header.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
+<div>
+    <Header />
+</div>
 {@render children()}
---
+++ src/routes/Header.svelte
@@ -0,0 +1,5 @@
+<header>
+    <div class="container mx-auto flex items-center justify-between p-4">
+        Aitra
+    </div>
+</header>
